864e34a69909d0fc0c400a910f48c71da35ef23d
phil85 <philipp.baumann@pqm.unibe.ch>
2023-01-12 16:48:04 +0100

    Refactoring to improve readability of code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pccc import pccc
 from sklearn.metrics import adjusted_rand_score
 
-# %% Generate data for illustrative example
+# %% Generate data for illustrative example from paper (https://arxiv.org/abs/2212.14437)
 
 # Define data
 data = [[0, 0, 0],
@@ -23,25 +23,31 @@ data = [[0, 0, 0],
         [6, 4, 2],
         [6, 6, 2]]
 
-df = pd.DataFrame(data, columns=['x1', 'x2', 'class'])
+df = pd.DataFrame(data, columns=['x1', 'x2', 'ground_truth_label'])
 
 #%% Define problem input parameters and sets
 
-n_clusters = df['class'].unique().size
+n_clusters = df['ground_truth_label'].unique().size
 X = df[['x1', 'x2']].values
-y = df['class'].values
+y = df['ground_truth_label'].values
 
 # %% Define information on pairs of objects
 
-ml = [(2, 6), (9, 10)]
-cl = [(2, 7), (6, 7), (5, 11)]
-sml = [(1, 2), (1, 6)]
-scl = [(4, 8), (8, 13)]
-sml_weights = [0.8, 0.6]
-scl_weights = [0.9, 0.5]
+hard_mustlink_constraints = [(2, 6), (9, 10)]
+hard_cannotlink_constraints = [(2, 7), (6, 7), (5, 11)]
+soft_mustlink_constraints = [(1, 2), (1, 6)]
+soft_cannotlink_constraints = [(4, 8), (8, 13)]
+confidence_levels_of_soft_mustlink_constraints = [0.8, 0.6]
+confidence_levels_of_soft_cannotlink_constraints = [0.9, 0.5]
 
 # %% Apply algorithm
-labels = pccc(X, n_clusters, ml=ml, cl=cl, sml=sml, scl=scl, sml_weights=sml_weights, scl_weights=scl_weights,
+labels = pccc(X, n_clusters,
+              ml=hard_mustlink_constraints,
+              cl=hard_cannotlink_constraints,
+              sml=soft_mustlink_constraints,
+              scl=soft_cannotlink_constraints,
+              sml_weights=confidence_levels_of_soft_mustlink_constraints,
+              scl_weights=confidence_levels_of_soft_cannotlink_constraints,
               random_state=24)
 
 # %% Evaluate assignment
